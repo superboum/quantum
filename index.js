3ea369b93fb835522cfa844646ca33386e06a4db
Quentin Dufour <quentin@dufour.tk>
2017-11-09 15:21:51 +0100

    socket.io support

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http);
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/a', (req, res) => res.render('index'))
+io.on('connection', socket => console.log('a user connected'))
 
-app.listen(3000, () => console.log('QUANTUM'))
+http.listen(3000, () => console.log('QUANTUM'))

2877dad88a6fb644476f7f32c4f72add09accf3f
Quentin Dufour <quentin@dufour.tk>
2017-11-14 14:28:03 +0100

    Polygon is now tested

--- test/polygon_test.js
+++ test/polygon_test.js
@@ -1,4 +1,25 @@
 import Polygon from '../public/js/foundation/polygon.js'
+import { assert } from 'chai'
 
-let p = new Polygon([{x: 5, y: 6}])
-console.log(p)
+describe('Polygon', () => {
+  describe('#edges', () => {
+    it('should compute edges', () => {
+      let p = new Polygon([
+        {x: 0, y: 0},
+        {x: 5, y: 1},
+        {x: 1, y: 5}
+      ])
+
+      let e = p.edges
+
+      assert.deepEqual(
+        p.edges,
+        [
+          {x: 5, y: 1},
+          {x: -4, y: 4},
+          {x: -1, y: -5}
+        ])
+    })
+  })
+})
+
